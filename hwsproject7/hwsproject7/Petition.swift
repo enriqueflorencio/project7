a12852273ceec4106c7ba421478475c47477199e
Enrique Florencio <45479678+enriqueflorencio@users.noreply.github.com>
2019-03-27 13:37:39 -0700

    Add files via upload

---
+++ hwsproject7/hwsproject7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  hwsproject7
+//
+//  Created by Daniel Florencio on 3/27/19.
+//  Copyright © 2019 Enrique Florencio. All rights reserved.
+//
+
+import Foundation
+
+struct Petition {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ hwsproject7/hwsproject7/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  hwsproject7
+//
+//  Created by Enrique Florencio on 3/27/19.
+//  Copyright © 2019 Enrique Florencio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        return cell
+    }
+
+
+}
+
